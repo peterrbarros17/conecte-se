b27266588814cc4a20a05817a4ba16b768bb96d4
peterrbarros17 <peterrbarros17@gmail.com>
2024-07-09 10:05:19 -0300

    switch s3 json to gist

--- src/api/fetchCreatorData.ts
+++ src/api/fetchCreatorData.ts
@@ -2,7 +2,7 @@ import { CreatorDataType } from "@/types/creatorDataType";
 
 export async function fetchCreatorData(): Promise<CreatorDataType> {
   const res = await fetch(
-    `https://creator-data.s3.us-east-2.amazonaws.com/${process.env.S3_AWS}.json`,
+    `https://gist.githubusercontent.com/peterrbarros17/409b13f0231a5a3b2ea2dbb5862c3039/raw/2386bd720578479b5b46f45f891ad2f86de4224d/${process.env.GIST}.json`,
     { next: { revalidate: 0 } }
   );
   if (!res.ok) console.log("Error");
